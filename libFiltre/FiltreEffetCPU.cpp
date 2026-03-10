b3d41dbadfbf502dc47d8620de984e28d780483b
jfiguinha <jfiguinha@hotmail.fr>
2026-03-10 11:36:40 +0100

    Update scale

--- libFiltre/FiltreEffetCPU.cpp
+++ libFiltre/FiltreEffetCPU.cpp
@@ -1101,12 +1101,7 @@ Mat CFiltreEffetCPU::Interpolation(const Mat& inputData, const int& widthOut, co
 		else if (method > 7)
 		{
 			int local_method = method - 7 + 1000;
-
-#ifdef TWO_PASS_SCALE
 			std::unique_ptr<C2PassScale> m_LocalFilter;
-#else
-			std::unique_ptr<CInterpolationBicubic> m_LocalFilter;
-#endif
 
 			switch (local_method)
 			{
